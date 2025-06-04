a60fabe62fb44f0c55d268f91e8c220a410b44ea
Adrian <adrian.piedade@gmail.com>
2025-06-04 20:29:26 -0300

    adiciona configuracoes de admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email',)
+    ordering = ('-id',)
+    list_filter = ('created_date',)
+    search_fields = ('id', 'first_name', 'last_name',)
+    list_per_page = 25
+    list_max_show_all = 250
+    list_editable = ('phone',)
+    list_display_links = ('id', 'first_name',)
