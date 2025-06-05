e42c4cc6c4a6b43cc4eca835216b6ef1cf13f07f
Adrian <adrian.piedade@gmail.com>
2025-06-05 00:17:54 -0300

    Adiciona novos templates e configs

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from contact.models import Contact
 
 def index(request):
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .order_by('first_name')[0:10]
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
     return render(
         request,
         'contact/index.html',
+        context=context
+    )
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))
+    contact_name = f"{single_contact.first_name} {single_contact.last_name} - "
+
+    context = {
+        'contact': single_contact,
+        'site_title': contact_name
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context=context
     )
